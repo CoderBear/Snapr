7e971b3f6e5687421934ae82680e8b1a61c86c16
Joe Ortega <joeortega79@gmail.com>
2018-03-26 10:37:49 +1030

    Can now successfully upload photos

---
+++ app/src/main/java/com/udemy/sbsapps/snapr/CreateSnapsActivity.kt
@@ -0,0 +1,94 @@
+package com.udemy.sbsapps.snapr
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import java.io.ByteArrayOutputStream
+import java.util.*
+
+
+class CreateSnapsActivity : AppCompatActivity() {
+
+    var createSnapImageView: ImageView? = null
+    var message: EditText? = null
+    var storage = FirebaseStorage.getInstance()
+    var imageName = UUID.randomUUID().toString() + ".jpg"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_snaps)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhoto()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val selectedImage = data!!.data
+
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                createSnapImageView = findViewById<ImageView>(R.id.imageView)
+
+                createSnapImageView?.setImageBitmap(bitmap)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    fun chooseImageClicked(view:View){
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            getPhoto()
+        }
+    }
+
+    private fun getPhoto() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, 1)
+    }
+
+    fun nextClicked(view: View){
+        // Get the data from an ImageView as bytes
+        createSnapImageView?.setDrawingCacheEnabled(true)
+        createSnapImageView?.buildDrawingCache()
+        val bitmap = createSnapImageView?.getDrawingCache()
+        val baos = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val uploadTask = FirebaseStorage.getInstance().getReference().child("images").child(imageName).putBytes(data)
+        uploadTask.addOnFailureListener(OnFailureListener {
+            // Handle unsuccessful uploads
+        }).addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+            // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+            val downloadUrl = taskSnapshot.downloadUrl
+            Log.i("URL", downloadUrl.toString())
+        })
+    }
+}
--- app/src/main/java/com/udemy/sbsapps/snapr/SnapsActivity.kt
+++ app/src/main/java/com/udemy/sbsapps/snapr/SnapsActivity.kt
@@ -1,12 +1,43 @@
 package com.udemy.sbsapps.snapr
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class SnapsActivity : AppCompatActivity() {
+
+    private val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snaps)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.snaps, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.createSnap) {
+            val intent = Intent(this, CreateSnapsActivity::class.java)
+            startActivity(intent)
+        } else if(item?.itemId == R.id.logout) {
+            mAuth.signOut()
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mAuth.signOut()
     }
 }
